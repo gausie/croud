910c979f5489d7ee02ac0758b34860ea7f59e11a
Sam Gaus <git@gaus.ie>
2015-02-27 16:12:15 +0000

    Fixed up Point submission

--- public/modules/points/config/points.client.config.js
+++ public/modules/points/config/points.client.config.js
@@ -5,6 +5,7 @@ angular.module('points').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Points', 'points', 'dropdown', '/points(/create)?');
-		Menus.addSubMenuItem('topbar', 'points', 'New Point', 'points/create');
+    Menus.addSubMenuItem('topbar', 'points', 'View Points', 'points');
+		Menus.addSubMenuItem('topbar', 'points', 'Create Point', 'points/create');
 	}
 ]);
--- public/modules/points/controllers/points.client.controller.js
+++ public/modules/points/controllers/points.client.controller.js
@@ -1,59 +1,65 @@
 'use strict';
 
 // Points controller
-angular.module('points').controller('PointsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Points', 'Campaigns',
+angular.module('points').controller('PointsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Points', 'Campaigns', 
 	function($scope, $stateParams, $location, Authentication, Points, Campaigns) {
 		$scope.authentication = Authentication;
-
     $scope.fields = {};
-
-    // Default settings for Leaflet.js map.
-    angular.extend($scope, {
-      center: {
-        lat: 51,
-        lng: 0,
-        zoom: 8
-      },
-      events: {},
-      markers: {}
-    });
     
-    // Set map marker on click.
-    $scope.$on('leafletDirectiveMap.click', function(event, args){
-      var leafEvent = args.leafletEvent;
-      
-      var marker = {
-        lat: leafEvent.latlng.lat,
-        lng: leafEvent.latlng.lng
-      };
-      
-      // Either create or update a single marker.
-      if ($scope.markers.length < 1) {
-        $scope.markers.push(marker);
-      } else {
-        $scope.markers[0] = marker;
-      }
-    });
+    // This will be inherited and altered by MapController.
+    $scope.marker = {};
 
 		// Create new Point
 		$scope.create = function() {
-			// Create new Point object
-      var location = this.markers[0];
+      var self = this;
+      
+      // Start validation.
+      var errors = [];
+      
+			// Validate that a location has been supplied.
+      if (this.marker.lat === undefined) {
+        errors.push("You must specify a location.");
+      }
+      
+      // Validate custom fields.
+      this.campaign.fields.forEach(function(field) {
+        console.dir(field);
+        if (field.required) {
+          if (self.fields[field.name] === undefined || self.fields[field.name] === "") {
+            errors.push("'" + field.name + "' is a required field.");
+          }
+        }
+      });
+      
+      // Finish validation
+      if (errors.length > 0) {
+          $scope.errors = errors;
+          return;
+      }
+      
+      // Create new Point object
 			var point = new Points ({
-        campaign: this.campaign,
-				location: [location.lat, location.lng]
+        campaign: this.campaign._id,
+				location: this.marker.array,
+        data: this.fields
 			});
 
 			// Redirect after save
 			point.$save(function(response) {
 				$location.path('points/' + response._id);
 
-				// Clear form fields
-				$scope.name = '';
+        // Reset the form
+        $scope.campaign = null;
+        $scope.$broadcast('resetMap');
+
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
 		};
+
+    $scope.$watch('campaign', function(v) {
+      $scope.$broadcast('resetMap');
+    });
 
 		// Remove existing Point
 		$scope.remove = function(point) {
