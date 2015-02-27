cabaaf79f7c8ad8ea70050497af4ca77d467d848
Sam Gaus <git@gaus.ie>
2015-02-27 16:10:44 +0000

    Changed Campaigns terminology

--- public/modules/campaigns/config/campaigns.client.config.js
+++ public/modules/campaigns/config/campaigns.client.config.js
@@ -5,7 +5,7 @@ angular.module('campaigns').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Campaigns', 'campaigns', 'dropdown', '/campaigns(/create)?');
-		Menus.addSubMenuItem('topbar', 'campaigns', 'List Campaigns', 'campaigns');
-		Menus.addSubMenuItem('topbar', 'campaigns', 'New Campaign', 'campaigns/create');
+		Menus.addSubMenuItem('topbar', 'campaigns', 'View Campaigns', 'campaigns');
+		Menus.addSubMenuItem('topbar', 'campaigns', 'Create Campaign', 'campaigns/create');
 	}
 ]);
