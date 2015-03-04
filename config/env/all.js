640c2e7d9df25137ce04d1b8fe8a6bab67ff9ca2
Sam Gaus <git@gaus.ie>
2015-03-04 15:14:39 +0000

    Moved to Font-Awesome

--- config/env/all.js
+++ config/env/all.js
@@ -15,7 +15,8 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-        'public/lib/leaflet/dist/leaflet.css'
+        'public/lib/leaflet/dist/leaflet.css',
+        'public/lib/font-awesome/css/font-awesome.css'
       ],
       js: [
         'public/lib/angular/angular.js',
--- config/env/production.js
+++ config/env/production.js
@@ -7,7 +7,8 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-        'public/lib/leaflet/dist/leaflet.css'
+        'public/lib/leaflet/dist/leaflet.css',
+        'public/lib/font-awesome/css/font-awesome.min.css'
       ],
       js: [
         'public/lib/angular/angular.min.js',
