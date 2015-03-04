fe5aef0488c4a309c6b688944fdc47efafdc026c
Sam Gaus <git@gaus.ie>
2015-03-04 20:33:23 +0000

    BUttons can be added to the menu (and one has\!)

--- public/modules/points/config/points.client.config.js
+++ public/modules/points/config/points.client.config.js
@@ -4,7 +4,7 @@
 angular.module('points').run(['Menus',
   function(Menus) {
     // Set top bar menu items
-    Menus.addMenuItem('topbar', 'Points', 'points', 'dropdown', '/points(/create)?');
+    Menus.addMenuItem('topbar', '<i class="fa fa-map-marker"></i> Add a point!', 'points/create', 'button', undefined, undefined, undefined, -1);
     Menus.addSubMenuItem('topbar', 'points', 'View Points', 'points');
     Menus.addSubMenuItem('topbar', 'points', 'Create Point', 'points/create');
   }
